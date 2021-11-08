6fd29af8774cda6cdf3887aabb915a62a5ad4b87
Kevin Guerrero <guerrk83z@gmail.com>
2021-11-08 15:37:20 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-function addingEventListener() {
+function clickAlert() {
+    alert("that tickles")
 }
+
+const input = document.getElementById('input');
+
+function addingEventListener() {
+    input.addEventListener('click', clickAlert);
+}
+
+addingEventListener()
+
